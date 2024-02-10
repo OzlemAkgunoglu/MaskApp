0d9cb49ba8c42c2766582759c04c0638011605ee
Ayhan <Ayhan@LAPTOP-S6H0KV0O>
2024-02-10 19:25:39 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference;
+
+
+namespace Business.Concrete
+{
+    public class PersonManager : IApplicantService
+    {
+        public void ApplyForMask(Person person)
+        {
+            
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+
+            return client.TCKimlikNoDogrulaAsync(new TCKimlikNoDogrulaRequest(new TCKimlikNoDogrulaRequestBody(person.NationalID,person.Name,person.LastName,person.BirthYear))).Result.Body.TCKimlikNoDogrulaResult;
+        }
+
+        public List<Person> GetAll()
+        {
+            return null;
+        }
+    }
+}
---
+++ Workspace/Program.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+Person person = new Person();
+
+person.Name = "ÖZLEM";
+person.LastName = "AKGÜNOĞLU";
+person.NationalID = 11215648945;
+person.BirthYear = 1998;
+
+PttManager pttManager=new PttManager(new PersonManager());
+pttManager.GiveMask(person);
+
+
+
+
